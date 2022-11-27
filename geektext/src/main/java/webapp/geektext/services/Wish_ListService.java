348e5d9140334583f280e6db201a665bc763f8d1
IrisRuiz309 <113149453+IrisRuiz309@users.noreply.github.com>
2022-11-26 21:55:10 -0500

    Add files via upload

---
+++ geektext/src/main/java/webapp/geektext/services/Wish_ListService.java
@@ -0,0 +1,31 @@
+package webapp.geektext.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import webapp.geektext.entities.Wish_List;
+import webapp.geektext.repos.Wish_ListRepo;
+
+import java.util.List;
+
+@Service
+public class Wish_ListService {
+
+    @Autowired Wish_ListRepo wishlistRepo;
+
+    public Wish_ListService(){
+
+    }
+
+    public ResponseEntity<List<Wish_List>> getWish_List() {
+        List<Wish_List> Wish_ListToReturn = wishlistRepo.findAll();
+
+        if(Wish_ListToReturn .isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else {
+            return new ResponseEntity<List<Wish_List>>(Wish_ListToReturn , HttpStatus.OK);
+        }
+    }
+}
