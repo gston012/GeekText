7b17de19fe8a45b560ec20370ab6a641fdb33307
IrisRuiz309 <113149453+IrisRuiz309@users.noreply.github.com>
2022-11-29 00:27:25 -0500

    Update Wish_ListService.java

--- geektext/src/main/java/webapp/geektext/services/Wish_ListService.java
+++ geektext/src/main/java/webapp/geektext/services/Wish_ListService.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import webapp.geektext.entities.Wish_List;
 import webapp.geektext.repos.Wish_ListRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class Wish_ListService {
 
-    @Autowired Wish_ListRepo wishlistRepo;
+	private final Wish_ListRepo wishlistRepo;
+	
+    @Autowired 
 
-    public Wish_ListService(){
+    public Wish_ListService(Wish_ListRepo wishlistRepo){
+    	this.wishlistRepo = wishlistRepo;
 
     }
 
@@ -28,4 +34,12 @@ public class Wish_ListService {
             return new ResponseEntity<List<Wish_List>>(Wish_ListToReturn , HttpStatus.OK);
         }
     }
+
+	public Optional<Wish_List> findByUser(String user) {
+		if(!wishlistRepo.existsById(user)) {
+			throw new ResponseStatusException (HttpStatus.NOT_FOUND, "User " + user + " does not has not created a wishlist");
+		}
+		// TODO Auto-generated method stub
+		return wishlistRepo.findById(user);
+	}
 }
