66640f6348046fc1af5c13c2938cc0cbacdbeedb
IrisRuiz309 <113149453+IrisRuiz309@users.noreply.github.com>
2022-11-29 00:25:08 -0500

    Update Wish_ListController.java

--- geektext/src/main/java/webapp/geektext/controllers/Wish_ListController.java
+++ geektext/src/main/java/webapp/geektext/controllers/Wish_ListController.java
@@ -3,20 +3,30 @@ package webapp.geektext.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import webapp.geektext.entities.Wish_List;
 import webapp.geektext.services.Wish_ListService;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+
+@RequestMapping("/wishlist")
 public class Wish_ListController {
 
-    @Autowired
-    Wish_ListService wishlistService ;
+    private final Wish_ListService wishlistService;
 
-    @GetMapping("/wishlist")
-    public ResponseEntity<List<Wish_List>> getWish_List() {
-        return wishlistService.getWish_List();
+    @Autowired
+    public Wish_ListController(Wish_ListService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+
+      @GetMapping("/{user}")
+    public Optional<Wish_List> findByUser(@PathVariable String user) {
+        return wishlistService.findByUser(user);
     }
 
 
